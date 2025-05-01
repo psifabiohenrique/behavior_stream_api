2a7c8de2837917f7b429ca20239e4e40c5b3b7ef
Fábio Henrique <psifabiohenrique@outlook.com>
2025-05-01 01:58:08 -0300

    Enhance admin interface and update User model string representation

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
+
 from .models.allowed_activity import AllowedActivity
+from .models.relationship import Relationship
+from .models.user import User
+
 
 @admin.register(AllowedActivity)
 class AllowedActivityAdmin(admin.ModelAdmin):
-    list_display = ('relationship__therapist', 'relationship__patient', 'activity_type', 'is_allowed')
-    list_filter = ('relationship__therapist', 'relationship__patient', 'activity_type')
-    search_fields = ('relationship__therapist__email', 'relationship__patient__email')
+    list_display = (
+        "relationship__therapist",
+        "relationship__patient",
+        "activity_type",
+        "is_allowed",
+    )
+    list_filter = (
+        "relationship__therapist",
+        "relationship__patient",
+        "activity_type",
+    )
+    search_fields = (
+        "relationship__therapist__email",
+        "relationship__patient__email",
+    )
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "role", "name")
+    list_filter = ("role", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Relationship)
+class RelationshipAdmin(admin.ModelAdmin):
+    list_display = ("therapist", "patient")
+    list_filter = ("therapist", "patient")
+    search_fields = ("therapist", "patient")
--- backend/core/models/user.py
+++ backend/core/models/user.py
@@ -45,4 +45,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ["name", "role"]
 
     def __str__(self):
-        return self.email
+        return f"{self.name} - {self.email}"
