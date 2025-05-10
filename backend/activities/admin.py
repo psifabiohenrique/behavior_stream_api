8a2901a6fb87f3549fea76e0934671b180961742
Fábio Henrique <psifabiohenrique@gmail.com>
2025-05-10 15:56:07 -0300

    refactor: Enhance JournalingAdmin configuration in admin panel

--- backend/activities/admin.py
+++ backend/activities/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .journaling.models import Journaling
+
+admin.site.register(Journaling)
+
+
+class JournalingAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "relationship")
+    list_filter = ("title", "date", "relationship")
+    search_fields = ("title", "date", "relationship", "resume", "emotion")
